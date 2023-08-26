48b4b9439cc37ae49745e4cdfda6d1af1d533a18
SuWeipeng <weipeng.su@outlook.com>
2023-08-26 11:09:23 +0800

    删除不用的 pip 库

--- modules/Youtube.py
+++ modules/Youtube.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
-import pyperclip as pc
 import time
 from threading import Thread
 
