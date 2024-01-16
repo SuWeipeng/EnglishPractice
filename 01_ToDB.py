50c2aaad80ccc61df9b3b0f23ff1029a6870fdfd
SuWeipeng <weipeng.su@outlook.com>
2024-01-16 16:24:53 +0800

    加入“片语”

--- 01_ToDB.py
+++ 01_ToDB.py
@@ -1,9 +1,9 @@
 # 输入文件名
-fileName       = 'ORCHARD3'
+fileName       = 'PHRASE'
 
 from modules.FileRead import FileRead
 
-vocabulary = ['IELTS1000','ORCHARD7','ORCHARD6','ORCHARD5','ORCHARD4','ORCHARD3']
+vocabulary = ['IELTS1000','ORCHARD7','ORCHARD6','ORCHARD5','ORCHARD4','ORCHARD3','PHRASE']
 listening  = ['Coversation01','SentenceStructure01','EnglishNews001','Medium01',
               'SimonReading_P1_01','SimonReading_P1_02']
 def main():
