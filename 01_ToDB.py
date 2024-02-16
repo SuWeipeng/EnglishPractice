7d4dd48e5fba3c6bd4f72e0437d1bbddbbb29fa8
SuWeipeng <weipeng.su@outlook.com>
2024-02-16 22:32:42 +0800

    01_ToDB supply AutoMode

--- 01_ToDB.py
+++ 01_ToDB.py
@@ -16,6 +16,7 @@ def loadMaterials():
             if len(configDict) > 0:
                 VocabularyList = configDict.get("vocabulary")
                 ListeningList  = configDict.get("listening")
+                AutoList       = configDict.get("auto")
     except (RuntimeError, IOError) as e:
         print(e)
         #out, err = self._proc.communicate()
@@ -28,16 +29,17 @@ if os.path.exists("config/materials.json"):
 
 vocabulary = VocabularyList
 listening  = ListeningList
+auto       = AutoList
 def main():
     file_type = None
     if fileName in vocabulary:
         file_type = 1
-    elif fileName in listening:
+    elif fileName in listening or fileName in auto:
         file_type = 2
     fr = FileRead(fileName, file_type)
     if fileName in vocabulary:
         fr.readContentToDB()
-    elif fileName in listening:
+    elif fileName in listening or fileName in auto:
         fr.listeningContentToDB()
 
 if __name__ == '__main__':
